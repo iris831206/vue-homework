2e406647c9879fa8ee893bdbac6497dab8807dbf
“iris <chidy76071919@hotmail.com>
2024-08-13 22:02:59 +0800

    week-1

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+    <RouterLink to="/">Home</RouterLink> |
+    <RouterLink to="/about">About</RouterLink> |
+    <RouterLink to="/week-1">week-1</RouterLink> |
+    <RouterLink to="/week-2">week-2</RouterLink>
+    <hr>
+    <HelloWorld />
+  </div>
+  <RouterView />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+
+</style>
