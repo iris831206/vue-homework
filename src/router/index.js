eaabe08b4cfd614d13a22043d519d489c35e3051
“iris <chidy76071919@hotmail.com>
2024-08-14 13:54:35 +0800

    fix-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/week-1',
+    component: () => import('../views/week-1.vue')
+  },
+  {
+    path: '/week-2',
+    component: () => import('../views/week-2.vue')
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/week-1',
-      component: () => import('../views/week-1.vue')
-    },
-    {
-      path: '/week-2',
-      component: () => import('../views/week-2.vue')
-    }
-  ]
+  history: createWebHashHistory(),
+  linkActiveClass: 'active',
+  routes
 })
 
 export default router
